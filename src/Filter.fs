578925e36e14373421110b4f33f1c0c297625c4e
jmhickman <jon_hickman@proton.me>
2023-10-22 16:21:35 -0500

    Added some Filter functions.

--- src/Filter.fs
+++ src/Filter.fs
@@ -46,3 +46,26 @@ module Filter =
         ///  
         static member public attributeIsValue attr value (res: LDAPSearchResult list) =
             List.filter (fun p -> (p.lDAPData.ContainsKey attr && p.lDAPData[attr] = value)) res
+
+
+        ///
+        /// <summary>
+        /// Filter LDAPSearchResults to return a single result. The result is a singleton List, to maintain
+        /// composability with the PrettyPrinter.
+        /// </summary>
+        /// <remarks>
+        /// I confess I don't understand the usefulness of this, but I'm including it anyway because I anticipate
+        /// someone will ask for it.
+        /// </remarks>
+        /// 
+        static member public justOne (res: LDAPSearchResult list) = [List.head res]
+
+
+        ///
+        /// <summary>
+        /// Filter LDAPSearchResults by the specific WHOLE config used. This is useful with <c>Tee</c>s, allowing the
+        /// results of different searches to be split off for processing within that <c>Tee</c> based on the config.
+        /// </summary>
+        /// 
+        static member public byConfig config (res: LDAPSearchResult list) =
+            List.filter (fun p -> p.searchConfig = config) res

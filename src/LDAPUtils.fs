200d6a5c74eb04648a9697524e22f0a042b7b7f9
Jon <nope@null>
2024-08-14 20:00:14 -0500

    massive flux

--- src/LDAPUtils.fs
+++ src/LDAPUtils.fs
@@ -1,77 +1,92 @@
 ﻿namespace Fiewport
 
+open System
+
 module LDAPUtils =     
-    open System
+
+    open System.Security.AccessControl
     open System.DirectoryServices.Protocols
     open System.Net
 
     open Types
-    ///
-    /// Creates a connection to the specified LDAP endpoint at the specified path.
-    /// Generally, this should be the FDQN:
-    /// LDAP://somedomain.tld</code>
-    /// or
-    /// LDAP://somdomain.tld/CN=Some,CN=Container,DC=somedomain,DC=tld</code>
-    /// 
-    // let internal getDomainConnection lDAPEndpoint username password = 
-    //     new DirectoryEntry(lDAPEndpoint, username, password)
-
-    
-    /// 
-    /// Creates a DirectorySearcher using an existing connection to an LDAP endpoint.
-    // let internal getDomainSearcher config domain =
-    //     new DirectorySearcher(domain, config.filter, config.properties, config.scope)
-    //     |> fun ds ->            
-    //         ds.SecurityMasks <- SecurityMasks.Dacl ||| SecurityMasks.Group ||| SecurityMasks.Owner
-    //         ds        
+    open LDAPConstants
 
 
-    let internal configureLDAPConnection ldapEndpoint username (password: string) =
-        let ldapIdentifier = LdapDirectoryIdentifier(ldapEndpoint)
-        let credential = NetworkCredential(username, password)
-        new LdapConnection(ldapIdentifier, credential)
-        
-        
-    let internal getLDAPSearcher config =
-        let searchRequest = SearchRequest(config.ldapDomain, config.filter, config.scope, config.properties)
-        let flags = SecurityDescriptorFlagControl(SecurityMasks.Dacl ||| SecurityMasks.Group ||| SecurityMasks.Owner)
-        searchRequest.Controls.Add(flags) |> ignore
-        searchRequest
+    let internal readyLDAPSearch config =
+        let ldapIdentifier = LdapDirectoryIdentifier(config.ldapHost) // does this work with ldap://?
+        let credential = NetworkCredential(config.username, config.password)
+        let connection = new LdapConnection(ldapIdentifier, credential)    
+        let searchRequest =
+            SearchRequest(config.ldapDN, config.filter, config.scope, config.properties)
+            |> fun sr ->
+                sr.Controls.Add(SecurityDescriptorFlagControl(SecurityMasks.Dacl ||| SecurityMasks.Group ||| SecurityMasks.Owner)) |> ignore
+                sr
+        (connection, searchRequest)
+
+    let internal matchKnownSids sid =        
+        if wellKnownSids.ContainsKey sid then wellKnownSids[sid]
+        else if networkSids.ContainsKey (sid.Split '-' |> Array.last) then networkSids[sid.Split '-' |> Array.last]
+        else sid    
     
-    ///
-    /// Helper to convert a TLD in the connection style into the LDAP style 
-    let internal deriveDistinguishedString (domainTld: string) =
-        domainTld[7..].Split('.')
-        |> Array.map (fun s -> $"""DC={s},""")
-        |> fun xs -> String.Join("", xs).Trim(',')
+    let internal getAccessFlags accessMask =
+        activeDirectoryRightsList
+        |> List.filter (fun enum -> (accessMask &&& int enum) = int enum)
+        |> List.map (fun enum -> enum.ToString())
+        |> String.concat ", "
+    
+
+    let decodeNtSecurityDescriptors bytes =
+        let descriptor = CommonSecurityDescriptor(false, false, bytes, 0) // need to check into these flags to make sure I'm not reading these incorrectly
+        [for dacl in descriptor.DiscretionaryAcl do yield dacl]
+        |> List.map (fun dacl ->
+            match dacl with
+            | :? CommonAce as common ->
+                let flags = getAccessFlags common.AccessMask
+                $"{matchKnownSids common.SecurityIdentifier.Value}--{flags}"
+            | _ -> "")
+        |> List.filter (fun p -> p <> "")
 
 
- 
-    /// Removes the four record attrs from the Map that appear in the 'root' of the record.
-    let private setRecordAttrs searchType searchConfig (map: Map<string, ADDataTypes>) =       
-        { searchType = searchType
-          searchConfig = searchConfig 
-          lDAPSearcherError = None
-          lDAPData = map }
+    let handleNtSecurityDescriptor (map: Map<string,ADDataTypes list>) =
+        match map.ContainsKey "ntsecuritydescriptor" with
+        | true ->
+            let ntBytes = map["ntsecuritydescriptor"]
+            let map = map.Remove "ntsecuritydescriptor"
+            match ntBytes with
+            | [ADBytes b] ->                
+                decodeNtSecurityDescriptors b
+                |> List.map (fun s -> s |> ADString)
+                |> fun strings -> map.Add ("ntsecuritydescriptor", strings)                
+            | _ -> map
+        | false -> map
     
     
-    ///
-    /// Processes SearchResults into LDAPSearchResults, placing in a Map only the attributes that appeared
-    /// in the given SearchResult.
-    /// 
-    let internal LDAPCoercer searchType searchConfig (searchResult: SearchResult) =
-        [for name in searchResult.Properties.PropertyNames do yield name.ToString ()]
-        |> List.fold(fun mapData attr -> mapData |> Map.add attr (unboxLDAPValue attr searchResult))
-               Map.empty<string, ADDataTypes>
-        |> setRecordAttrs searchType searchConfig
+    let handleObjectGuid (map: Map<string, ADDataTypes list>) =
+        match map.ContainsKey "objectguid" with // should always have one, but w/e
+        | true ->
+            let guidBytes = map["objectguid"]
+            let map = map.Remove "objectguid"
+            match guidBytes with
+            | [ADBytes b] ->
+                [$"{b |> Guid}" |> ADString] |> fun strings -> map.Add("objectguid", strings)
+            | _ -> map
+        | false -> map
     
     
-    ///
-    /// Error message injection
-    let internal decodeLDAPSearcherError error =
-        match error with
-        | ServerConnectionError s -> "ServerConnectionError: " + s + " Check your connection string and user/pass"
-        | UnknownError80005000 s -> "UnknownError80005000: " + s
-        | NoSuchObject s -> "NoSuchObject: " + s
-        | InvalidDNSyntax s -> "InvalidDNSyntax: " + s
-        | OtherError s -> "OtherError: " + s
+    let createLDAPSearchResults (searchType: LDAPSearchType) config (results: Result<SearchResponse, string>) =
+        match results with
+        | Ok res ->
+            let ldapData = // need to handle objectsid, ntsecdes, objectguid, usercert
+                [for item in res.Entries do yield item] // Extract entries
+                |> List.collect (fun p ->
+                    [for name in p.Attributes.AttributeNames do yield name.ToString()]// extract the names of the attributes returned
+                    |> List.map (fun name -> (name, [for item in p.Attributes[name] do yield (item :?> byte array) |> ADBytes]))) // for each attribute, retrieve all values and create a tuple
+                |> List.fold (fun acc (name, values) -> acc |> Map.add name values) Map.empty<string,ADDataTypes list> // create a Map to easily access values
+                |> handleNtSecurityDescriptor
+                |> handleObjectGuid
+            
+        | Error e ->
+            { searchType = searchType
+              searchConfig = config
+              ldapSearcherError = e |> Some
+              ldapData = Map.empty<string,string list> }

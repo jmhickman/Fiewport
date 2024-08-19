73a81e2b7af057f27038c84fb1e9e36ea4678c68
Jon <nope@null>
2024-08-18 20:03:36 -0500

    Packaging updates

--- ImportFiewport.fsx
+++
@@ -1,8 +0,0 @@
-﻿//#r """PATH_TO_FIEWPORT"""
-#r "nuget: EluciusFTW.SpectreCoff"
-#r "nuget: System.DirectoryServices.Protocols"
-#r "nuget: MessagePack"
-#r "nuget: MessagePack.FSharpExtensions"
-
-open Fiewport
-open System.DirectoryServices.Protocols

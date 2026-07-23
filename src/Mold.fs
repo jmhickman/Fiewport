2ad9592bf77f8f50fdc75dbf6d29a5a90cde3b75
jmhickman <no@null.com>
2026-07-23 15:47:07 +0000

    Added docstring for Mold

--- src/Mold.fs
+++ src/Mold.fs
@@ -2,7 +2,19 @@
 
 module Mold =
 
-
+    ///
+    /// <summary>
+    /// <para><c>Mold.getKeys</c> allows the user to directly access the keys that were
+    /// returned in a given search. This is a deeply nested string list with the same
+    /// shape as the original results, just with the values omitted.</para>
+    /// <para>Example:</para>
+    /// <code>[config]
+    /// |> Searcher.getUsers
+    /// |> Mold.getKeys
+    /// |> someCustomFunction
+    /// |> ignore</code>
+    /// </summary>
+    ///
     let getKeys results =
         results
         |> List.map(fun result ->
@@ -12,6 +24,19 @@ module Mold =
                 |> List.map(fun map -> [for key in map.Keys do yield key]))
 
 
+    ///
+    /// <summary>
+    /// <para><c>Mold.getValues</c> allows the user to directly access the values that were
+    /// returned in a given search. This is a deeply nested string list with the same
+    /// shape as the original results, just with the keys omitted.</para>
+    /// <para>Example:</para>
+    /// <code>[config]
+    /// |> Searcher.getUsers
+    /// |> Mold.getValues
+    /// |> someCustomFunction
+    /// |> ignore</code>
+    /// </summary>
+    ///
     let getValues results =
         results
         |> List.map(fun result ->
@@ -22,6 +47,20 @@ module Mold =
                     [for key in map.Keys do yield map[key]]))
 
 
+
+    ///
+    /// <summary>
+    /// <para><c>Mold.composeResults</c> allows the user to directly access the keys and values
+    /// that were returned in a given search. This is a list-ified version of data with the
+    /// same shape as the original results.</para>
+    /// <para>Example:</para>
+    /// <code>[config]
+    /// |> Searcher.getUsers
+    /// |> Mold.composeResults
+    /// |> someCustomFunction
+    /// |> ignore</code>
+    /// </summary>
+    ///
     let composeResults results =
        results
         |> List.collect(fun result ->
